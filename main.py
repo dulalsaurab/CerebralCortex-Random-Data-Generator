070f63a07ccf4b6cfa58f1a06defa29edadf9237
nasirali1 <nasir.ali08@gmail.com>
2022-05-13 16:42:21 -0500

    made all timestamps aligned

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import datetime
 
 from ccrdg.battery_data import gen_battery_data
 from ccrdg.accel_gyro_data import gen_accel_gyro_data
-from ccrdg.location_data import gen_location_datastream, gen_semantic_location_datastream
+from ccrdg.location_data import gen_location_data#gen_location_datastream, gen_semantic_location_datastream
 import argparse
 from cerebralcortex.kernel import Kernel
 
@@ -30,16 +30,19 @@ def run():
 
 
     battery_stream_name = "org.md2k--{}--{}--battery--phone".format(study_name,user_id)
-    location_stream_name = "org.md2k--{}--{}--gps--phone".format(study_name,user_id)
+    gps_stream_name = "org.md2k--{}--{}--gps--phone".format(study_name,user_id)
     semantic_location_stream_name = "org.md2k--{}--{}--data_analysis--gps_episodes_and_semantic_location".format(study_name,user_id)
     accel_stream_name = "org.md2k.phonesensor--{}--{}--accelerometer--phone".format(study_name,user_id)
     gyro_stream_name = "org.md2k.phonesensor--{}--{}--gyroscope--phone".format(study_name,user_id)
 
-    gen_battery_data(CC, study_name=study_name, user_id=user_id, stream_name=battery_stream_name, start_time=start_time, end_time=end_time)
-    gen_location_datastream(CC, study_name=study_name, user_id=user_id, stream_name=location_stream_name, start_time=start_time, end_time=end_time)
-    gen_semantic_location_datastream(CC, study_name=study_name, user_id=user_id, stream_name=semantic_location_stream_name, start_time=start_time, end_time=end_time)
-    gen_accel_gyro_data(CC, study_name=study_name, user_id=user_id, stream_name=accel_stream_name, start_time=start_time, end_time=end_time)
-    gen_accel_gyro_data(CC, study_name=study_name, user_id=user_id, stream_name=gyro_stream_name, start_time=start_time, end_time=end_time)
+
+    gen_location_data(CC, study_name=study_name, user_id=user_id, gps_stream_name=gps_stream_name, location_stream_name=semantic_location_stream_name, start_time=start_time, end_time=end_time)
+    #gen_location_datastream(CC, study_name=study_name, user_id=user_id, stream_name=location_stream_name, start_time=start_time, end_time=end_time)
+    #gen_semantic_location_datastream(CC, study_name=study_name, user_id=user_id, stream_name=semantic_location_stream_name, start_time=start_time, end_time=end_time)
+
+    # gen_battery_data(CC, study_name=study_name, user_id=user_id, stream_name=battery_stream_name, start_time=start_time, end_time=end_time)
+    # gen_accel_gyro_data(CC, study_name=study_name, user_id=user_id, stream_name=accel_stream_name, start_time=start_time, end_time=end_time)
+    # gen_accel_gyro_data(CC, study_name=study_name, user_id=user_id, stream_name=gyro_stream_name, start_time=start_time, end_time=end_time)
 
 if __name__ == "__main__":
     run()
